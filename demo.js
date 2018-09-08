98bf9c850dc1ad8f1d55e27028ed18ed9d9d9f2d
alert8@qq.com <alert8@qq.com>
2018-09-08 09:57:52 +0800

    alert9
    修改图片地址

--- demo.js
+++ demo.js
@@ -11,33 +11,33 @@ const ap4 = new APlayer({
         name: '童话镇',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E7%AB%A5%E8%AF%9D%E9%95%87.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2'
     }, {
         name: '阿婆说',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E9%98%BF%E5%A9%86%E8%AF%B4.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/darling.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#46718b'
     }, {
         name: '弦上有春秋',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E5%BC%A6%E4%B8%8A%E6%9C%89%E6%98%A5%E7%A7%8B.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/yourname.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
       
         theme: '#505d6b'
     }, {
         name: '剑与家园',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E5%89%91%E4%B8%8E%E5%AE%B6%E5%9B%AD.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2',
     }
     , {
         name: '红色高跟鞋(川话版)',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E7%BA%A2%E8%89%B2%E9%AB%98%E8%B7%9F%E9%9E%8B%28%E5%B7%9D%E8%AF%9D%E7%89%88%29.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2',
     }
 
@@ -46,7 +46,7 @@ const ap4 = new APlayer({
         name: '克卜勒',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20%E5%85%8B%E5%8D%9C%E5%8B%92.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2',
     }
 
@@ -55,7 +55,7 @@ const ap4 = new APlayer({
         name: 'cant take my eyes off you',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20can%27t%20take%20my%20eyes%20off%20you.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2',
     }
 
@@ -64,7 +64,7 @@ const ap4 = new APlayer({
         name: 'New Soul',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20New%20Soul.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2',
     }
 
@@ -73,7 +73,7 @@ const ap4 = new APlayer({
         name: 'Bizarre Love Triangle',
         artist: '陈一发儿',
         url: 'http://zxmoa.qiniudn.com/%E9%99%88%E4%B8%80%E5%8F%91%E5%84%BF%20-%20Bizarre%20Love%20Triangle.mp3',
-        cover: 'https://moeplayer.b0.upaiyun.com/aplayer/hikarunara.jpg',
+        cover: 'http://p2.music.126.net/Xqbgefo-AsjWj4xqdhVO8g==/18725782534879488.jpg?param=200y200',
         theme: '#ebd0c2',
     }
 
